0c04eeba583b78ca59981d4b74ba165a0ec80981
Limstar12 <96039257+Limstar12@users.noreply.github.com>
2022-03-18 18:25:53 +0900

    Delete db directory

--- db/dbconfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host : 'database-1.cuaahtszwr5e.ap-northeast-2.rds.amazonaws.com',
-    user : 'admin',
-    password : 'tomato35!',
-    port : '3306',
-    database : 'react_suppose'
-}
