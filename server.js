c75bda859d650cb97ca30b8c3c6e1b19e86bc43b
Limstar12 <phji0326@gmail.com>
2022-03-18 16:31:38 +0900

    노드 구동 완료(정상구동)

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express();
+
+app.get('/', (req,res,next) => {
+    res.send('출력되라되라되라')
+})
+
+app.listen(5000,() => {
+    console.log('이거는 콘솔창에만 나오는거');
+})
